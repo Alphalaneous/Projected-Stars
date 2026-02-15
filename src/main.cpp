1a805800ad1052b00c056e4047168fa4863a0ffe
Ashton <38200084+Alphalaneous@users.noreply.github.com>
2026-02-14 21:25:14 -0500

    Beta 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/LevelInfoLayer.hpp>
-#include "ParticleHelper.hpp"
 
 using namespace geode::prelude;
 
@@ -8,99 +7,102 @@ class $modify(LevelInfoLayer) {
 	
 	bool init(GJGameLevel* level, bool a1) {
 		if (!LevelInfoLayer::init(level, a1)) return false;
+		if (level->m_stars == 0) return true;
 
-		CCSprite* progressBar;
 		CCSprite* progressInside;
+
 		if (!level->isPlatformer()) {
-			progressBar = typeinfo_cast<CCSprite*>(getChildByID("normal-mode-bar"));
+			auto progressBar = typeinfo_cast<CCSprite*>(getChildByID("normal-mode-bar"));
 			if (!progressBar) return true;
+
 			progressInside = progressBar->getChildByType<CCSprite*>(0);
 			if (!progressInside) return true;
 		}
 
-		if (level->m_stars == 0) return true;
-
-		float units = 0;
-		bool showStarSprite = false;
-		bool showOutline = false;
-		int count = 0;
 		int countToAdd = 0;
 
-		auto winSize = CCDirector::sharedDirector()->getWinSize();
-		std::string spriteName;
-		std::string spriteOutlineName;
+		auto winSize = CCDirector::get()->getWinSize();
 
-		auto projectedStarsLabel = CCLabelBMFont::create("-", "bigFont.fnt");
-		cocos2d::CCDictionary* statDict = GameStatsManager::sharedState()->m_playerStats;
+		auto statDict = GameStatsManager::get()->m_playerStats;
 
 		if (level->m_normalPercent != 100) {
 			countToAdd = level->m_stars;
 		}
 
-		projectedStarsLabel->setColor(ccWHITE);
+		auto projectedStarsLabel = CCLabelBMFont::create("-", "bigFont.fnt");
+		projectedStarsLabel->setColor({255, 255, 255});
 		projectedStarsLabel->setAnchorPoint({0, 0.5});
 		projectedStarsLabel->setPosition({ (winSize.width / 2) + 133.5f, 136 });
 		projectedStarsLabel->setZOrder(10);
 		projectedStarsLabel->setScale(0.4f);
 		projectedStarsLabel->setID("stars-label"_spr);
 
+		std::string value = level->isPlatformer() ? statDict->valueForKey("28")->getCString() : statDict->valueForKey("6")->getCString();
+		int count = numFromString<int>(value).unwrapOr(0);
+		float units = level->isPlatformer() ? 232 : level->m_normalPercent * (234.0f/100.0f);
+
+		bool showStarSprite = false;
+		bool showOutline = false;
+
 		if (!level->isPlatformer()) {
-			std::string value = statDict->valueForKey("6")->m_sString;
-			if(value.empty()) count = 0;
-			else count = std::stoi(value);
-			spriteName = "GJ_bigStar_001.png";
-			spriteOutlineName = "starOutline.png"_spr;
-			units = level->m_normalPercent * (234.0f/100.0f);
 			showStarSprite = true;
 
 			if (level->m_normalPercent == 100) {
 				projectedStarsLabel->setColor(ccYELLOW);
 				progressInside->setColor(ccYELLOW);
 			}
-			if (level->m_normalPercent <= 90) showOutline = true;
+			if (level->m_normalPercent <= 90) {
+				showOutline = true;
+			}
 		}
 		else {
-			std::string value = statDict->valueForKey("28")->m_sString;
-			if(value.empty()) count = 0;
-			else count = std::stoi(value);
-			spriteName = "GJ_bigMoon_001.png";
-			spriteOutlineName = "moonOutline.png"_spr;
-			units = 232;
-
-			if (level->m_normalPercent == 100) showStarSprite = true;
-			else showOutline = true;
+			if (level->m_normalPercent == 100) {
+				showStarSprite = true;
+			}
+			else {
+				showOutline = true;
+			}
 		}
-		std::string starCountStr = std::to_string(count + countToAdd);
 
-		CCSprite* starSpriteOutline = CCSprite::create(spriteOutlineName.c_str());
+		std::string type = level->isPlatformer() ? "Moon" : "Star";
+		std::string spriteName = fmt::format("GJ_big{}_001.png", type);
+		std::string spriteOutlineName = fmt::format("{}Outline.png"_spr, utils::string::toLower(type));
+
+		auto starSpriteOutline = CCSprite::create(spriteOutlineName.c_str());
 		starSpriteOutline->setPosition({ (winSize.width / 2) + 118, 136});
 		starSpriteOutline->setAnchorPoint({ 0.5f, 0.5f });
 		starSpriteOutline->setScale(0.5f);
 		starSpriteOutline->setZOrder(3);
 		starSpriteOutline->setID("stars-sprite-outline"_spr);
 
-		projectedStarsLabel->setString(starCountStr.c_str());
+		projectedStarsLabel->setString(numToString(count + countToAdd).c_str());
 
 		float particleAmount = level->m_normalPercent;
 
 		if (level->m_normalPercent < 5 && !level->isPlatformer()) particleAmount = 5;
+		if (level->isPlatformer()) particleAmount = 50;
 
-		CCSprite* starSprite = CCSprite::createWithSpriteFrameName(spriteName.c_str());
+		auto starSprite = CCSprite::createWithSpriteFrameName(spriteName.c_str());
 		starSprite->setPosition({ (winSize.width / 2) - 118 + units, 136});
 		starSprite->setAnchorPoint({ 0.5f, 0.5f });
 		starSprite->setScale(0.5f);
 		starSprite->setZOrder(3);
 		starSprite->setID("stars-sprite"_spr);
 
-		if (CCParticleSystem* particles = ParticleHelper::createStarParticles(particleAmount)) {
-			particles->setPosition({ (winSize.width / 2) - 118 + units, 136});
-			particles->setAnchorPoint({ 0.5f, 0.5f });
-			particles->setScale(0.5f);
-			particles->setZOrder(3);
-			particles->setID("stars-particles"_spr);
-			addChild(particles);
-		}
-		
+		int particleID = level->isPlatformer() ? 101 : 4;
+
+		auto particles = GameToolbox::particleFromString(fmt::format("100a-1a1a1a33a0a360a20a5a10a10a0a0a0a0a0a0a15a5a0a360a1a1a1a1a1a1a1a0a5a0a0a360a1a1a1a1a1a1a0a0a0a0a0a0a100a0a0a0a0a0a0a2a1a0a0a0a{}a0a0a0a0a0a0a0a0a0a0a5a0a5a0", particleID).c_str(), CCParticleSystemQuad::create(), false);
+		particles->setTotalParticles(particleAmount / 2);
+
+		auto offset = level->isPlatformer() ? CCPoint{-3 , -1} : CCPoint{0, 0};
+
+		particles->setPosition(CCPoint{(winSize.width / 2) - 118 + units , 136} + offset);
+		particles->setAnchorPoint({ 0.5f, 0.5f });
+		particles->setScale(0.5f);
+		particles->setZOrder(3);
+		particles->setID("stars-particles"_spr);
+		addChild(particles);
+
 		starSprite->setVisible(showStarSprite);
 		starSpriteOutline->setVisible(showOutline);
 
